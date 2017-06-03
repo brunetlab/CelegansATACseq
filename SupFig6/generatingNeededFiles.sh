5abe507c26c77b2c0560fcdd253d693630265de1
Aaron C Daugherty <acdaugherty@users.noreply.github.com>
2017-06-03 16:51:31 -0700

    Moving SupFig4 script to appropriate folder

--- SupFig6/generatingNeededFiles.sh
+++
@@ -1,1 +0,0 @@
-intersectBed -a <single bp score bedGraph> -b <region of interest bed> | coverageBed -a - -b <region of interest bed> | awk '{if($NF > 0.5) print $0}' | sortBed -i - | mapBed -a - -b <single bp score bedGraph> -c 4 -o mean  
