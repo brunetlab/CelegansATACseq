50546fbbde0beae1b9f9eadd0536573740733338
Aaron C Daugherty <acdaugherty@users.noreply.github.com>
2017-06-04 15:29:16 -0700

    Update generatingNeededFiles.sh

--- SupFig4/generatingNeededFiles.sh
+++ SupFig4/generatingNeededFiles.sh
@@ -1,1 +1,6 @@
-intersectBed -a <single bp score bedGraph> -b <region of interest bed> | coverageBed -a - -b <region of interest bed> | awk '{if($NF > 0.5) print $0}' | sortBed -i - | mapBed -a - -b <single bp score bedGraph> -c 4 -o mean  
+# This is just an example of what was done
+intersectBed -a <single bp score bedGraph> -b <region of interest bed> | \
+coverageBed -a - -b <region of interest bed> | \
+awk '{if($NF > 0.5) print $0}' | \
+sortBed -i - | \
+mapBed -a - -b <single bp score bedGraph> -c 4 -o mean  
